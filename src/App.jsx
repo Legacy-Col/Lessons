e3338e877d7e7b10880c8d87c85a98a4aff7cfca
Legacy-Col <lynxlestley@gmail.com>
2025-01-18 10:14:02 -0800

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Lesson1 from './Components/Lesson1'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='font-serif'>
+      <h1>Hello Guys!</h1>
+      <Lesson1 />
+    </div>
   )
 }
 
---
+++ src/Components/Lesson1.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Lesson1 = () => {
+  return (
+    <div>Pizza</div>
+  )
+}
+
+export default Lesson1
